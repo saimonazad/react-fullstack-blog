a744912188efbf2c1e838cc4a25991683a965a63
saimon <saimon.azad@hotmail.com>
2020-09-18 20:28:36 +0600

    basic server setup done

--- config/config.js
+++ config/config.js
@@ -1,7 +1,9 @@
-export default config = {
+const config = {
     env: process.env.NODE_ENV || 'development',
     port: process.env.PORT || 3000,
     mongoUri: process.env.MONGODB_URI || 'mongodb://mongodb0.example.com:27017/react-fullstack-blog.user',
     mongoOptions: { useNewUrlParser: true, useCreateIndex: true, useUnifiedTopology: true },
-    jstSecret: process.env.JWT_SECRET || '2020@azad2##*$'
+    jwtSecret: process.env.JWT_SECRET || '2020@azad2##*$'
 }
+
+export default config;
--- server/express.js
+++ server/express.js
@@ -1,15 +1,20 @@
 import express from 'express'
 import bodyParser from 'body-parser'
 import cookieParser from 'cookie-parser'
-import compress from 'compress'
 import cors from 'cors'
 import helmet from 'helmet'
 
+const app = express()
+
+
 app.use(bodyParser.json())
 app.use(bodyParser.urlencoded({ extended: true }))
-app.use(cookieParser);
+app.use(cookieParser())
 app.use(helmet())
-app.use(compress())
 app.use(cors())
 
-export default app
+app.get('*', (req, res) => {
+    res.status(200).send("hello api");
+});
+
+export default app;
