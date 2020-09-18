de5d66740b38ba65b0a64740831f1c0806ce5f3f
saimon <saimon.azad@hotmail.com>
2020-09-18 20:05:05 +0600

    db connection code added

--- config/config.js
+++ config/config.js
@@ -2,5 +2,6 @@ export default config = {
     env: process.env.NODE_ENV || 'development',
     port: process.env.PORT || 3000,
     mongoUri: process.env.MONGODB_URI || 'mongodb://mongodb0.example.com:27017/react-fullstack-blog.user',
+    mongoOptions: { useNewUrlParser: true, useCreateIndex: true, useUnifiedTopology: true },
     jstSecret: process.env.JWT_SECRET || '2020@azad2##*$'
 }
--- server/server.js
+++ server/server.js
@@ -1,7 +1,12 @@
-import config from '../config/config';
+import config from './config/config';
 import app from './express'
 import mongoose from 'mongoose'
 
+mongoose.Promise = global.Promise;
+mongoose.connect(config.mongoUri, config.mongoOptions)
+mongoose.connection.on('error', () => {
+    throw new Error(`unable to connect to database: ${config.mongoUri}`)
+})
 
 app.listen(config.port, (err) => {
     if (err) console.log(err)
